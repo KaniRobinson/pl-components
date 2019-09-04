f194ce52b40317f761c429f90947a2d9c8f5c851
KaniRobinson <kani_westham@hotmail.com>
2019-09-04 17:33:54 +0100

    Added button component

---
+++ components/button/Button.vue
@@ -0,0 +1,83 @@
+<template>
+  <button
+    :disabled="loading"
+    :class="classes"
+    v-on="$listeners"
+  >
+    <i v-if="loading" class="fas fa-circle-notch fa-spin" />
+    <span v-else>
+      <i v-if="icon" :class="icon" class="mr-2" />
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PButton',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    background: {
+      type: String,
+      default: 'transparent',
+    },
+    hover: {
+      type: String,
+      default: 'transparent',
+    },
+    color: {
+      type: String,
+      default: 'gray-900',
+    },
+    pill: {
+      type: Boolean,
+      default: true,
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: [String,null],
+      default: null,
+    },
+  },
+  computed: {
+    classes () {
+      return {
+        'flex': true,
+        'items-center': true,
+        'justify-center': true,
+        'py-4': true,
+        'px-6': true,
+        'rounded-full': this.pill,
+        'text-center': true,
+        'text-sm': true,
+        'no-underline': true,
+        'leading-tight': true,
+        'cursor-pointer': true,
+        'relative': true,
+        'text-center': true,
+        'select-none': true,
+        'focus:no-underline': true,
+        'focus:outline-none': true,
+        'border': this.outline,
+        'border-solid': this.outline,
+        [`hover:text-white`]: this.outline,
+        [`hover:bg-${this.background}`]: this.outline,
+        [`border-${this.background}`]: this.outline,
+        [`bg-${this.background}`]: !this.outline,
+        [`text-${this.color}`]: true,
+        [`hover:bg-${this.hoverBackground}`]: !this.outline,
+      }
+    },
+  },
+}
+</script>
--- components/index.js
+++ components/index.js
@@ -1,1 +1,12 @@
-export { default as Input } from './input/Input'
+export { default as Button } from './button/Button'
+export { default as Checkbox } from './checkbox/Checkbox'
+export { default as Radio } from './radio/Radio'
+// export { default as Input } from './input/Input'
+// export { default as Textarea } from './textarea/Textarea'
+// export { default as Autocomplete } from './autocomplete/Autocomplete'
+// export { default as Select } from './select/Select'
+// export { default as Datepicker } from './datepicker/Datepicker'
+// export { default as Timepicker } from './timepicker/Timepicker'
+// export { default as Dropdown } from './dropdown/Dropdown'
+// export { default as Upload } from './upload/Upload'
+
