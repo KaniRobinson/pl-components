ba419c86d178b7b7342e1c6ac27cd2ef4f06cf52
KaniRobinson <kani_westham@hotmail.com>
2019-09-05 23:22:56 +0100

    Added Card Element

---
+++ components/card/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="classes">
+    <img v-if="image" :class="imageClasses" :src="image" alt="image" />
+    <div :class="contentClasses">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PCard',
+  props: {
+    image: {
+      type: [String,null],
+      default: null,
+    },
+    color: {
+      type: String,
+      default: 'white',
+    },
+  },
+  computed: {
+    classes () {
+      return {
+        'rounded': true,
+        'shadow': true,
+        [`bg-${this.color}`]: true,
+      }
+    },
+    contentClasses () {
+      return {
+        'px-6': true,
+        'py-4': true,
+      }
+    },
+    imageClasses () {
+      return {
+        'rounded-t': true,
+        'w-full': true,
+      }
+    },
+  },
+}
+</script>
