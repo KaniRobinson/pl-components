02b22e86da4e319189d2d9bbe30b076c51b7dfe5
KaniRobinson <kani_westham@hotmail.com>
2019-09-05 23:23:31 +0100

    Added Tag component

---
+++ components/tag/Tag.vue
@@ -0,0 +1,35 @@
+<template>
+  <span :class="classes">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'PTag',
+  props: {
+    color: {
+      type: String,
+      default: 'gray-200',
+    },
+    textColor: {
+      type: String,
+      default: 'gray-700',
+    }
+  },
+  computed: {
+    classes () {
+      return {
+        'inline-block': true,
+        'rounded-full': true,
+        'px-3': true,
+        'py-1': true,
+        'text-sm': true,
+        'font-semibold': true,
+        [`bg-${this.color}`]: true,
+        [`text-${this.textColor}`]: true,
+      }
+    },
+  },
+}
+</script>
