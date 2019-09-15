19b746be82fcf5f69020f5369bc47389c94c267a
KaniRobinson <kani_westham@hotmail.com>
2019-09-15 22:09:45 +0100

    Added Pricing component

---
+++ components/pricing/Pricing.vue
@@ -0,0 +1,45 @@
+<template>
+  <p-row>
+    <p-column-2
+      v-for="(item,index) in items"
+      :key="index"
+      class="mb-4">
+      <p-card>
+        <p-tag
+          class="mb-4"
+          :color="`${item.color}-300`"
+          :textColor="`${item.color}-700`">
+          {{ item.type }}
+        </p-tag>
+        <div class="flex items-baseline mb-4">
+          <p class="text-4xl">{{ item.price }}</p>
+          <p class="text-gray-600 ml-2">/ {{ item.per }}</p>
+        </div>
+        <p class="text-gray-600 mb-4">{{ item.caption }}</p>
+        <ul class="list-reset mb-4 mx-2">
+          <li v-for="(addon,index) in item.items" :key="index" class="text-gray-600 mb-2">
+            <i class="far fa-check-circle text-green-500 mr-2" /> {{ addon }}
+          </li>
+        </ul>
+        <p-button
+          background="blue-600"
+          color="white"
+          class="w-full">
+          {{ item.button }}
+        </p-button>
+      </p-card>
+    </p-column-2>
+  </p-row>
+</template>
+
+<script>
+export default {
+  name: 'PPricing',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
