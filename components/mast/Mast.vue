acfe58a69d3dc59fb94fc55e7dfb417bf895d33c
KaniRobinson <kani_westham@hotmail.com>
2019-09-15 22:09:28 +0100

    Created Mast component

---
+++ components/mast/Mast.vue
@@ -0,0 +1,69 @@
+<template>
+  <section :class="classes">
+    <div class="container max-w-3xl mx-auto">
+      <p :class="tagClasses">{{ tag }}</p>
+      <p :class="titleClasses">{{ title }}</p>
+      <p :class="captionClasses">{{ caption }}</p>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PMast',
+  props: {
+    tag: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    classes () {
+      return {
+        'bg-gray-800': true,
+        'pt-12': true,
+        'pb-24': true,
+      }
+    },
+    tagClasses () {
+      return {
+        'text-white': true,
+        'text-xs': true,
+        'uppercase': true,
+        'py-1': true,
+        'px-4': true,
+        'rounded': true,
+        'border': true,
+        'border-white': true,
+        'table': true,
+        'mx-auto': true,
+        'mb-4': true,
+      }
+    },
+    titleClasses () {
+      return {
+        'text-white': true,
+        'text-center': true,
+        'text-4xl': true,
+        'mb-4': true,
+      }
+    },
+    captionClasses () {
+      return {
+        'text-gray-400': true,
+        'text-center': true,
+        // 'text-sm': true,
+      }
+    },
+  },
+}
+</script>
